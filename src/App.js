93f3ce264c272fd621a562417e5a2beb69396579
Fluffy-Lion <leoncooper31@gmail.com>
2021-01-15 15:16:19 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state ={
+    tasks: ["task 1", "task 2"],
+    input:''
+  }
+  addTask = (event) => {
+    
+    this.setState({ input: event.target.value})
+    
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      tasks: [...this.state.tasks, this.state.input]
+    })
+  }
+  // changeHandler = event => {
+  //   this.setState({input: event.target.value})
+  // }
+ removeTaskHandler = index => {
+   let storedTasks = [...this.state.tasks]
+   storedTasks.splice(index, 1)
+   this.setState({ tasks: storedTasks})
+ }
+ changeHandler = event => {
+   this.setState({ input: event.target.value })
+ }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>add task</h2>
+     <Card tasks={this.state.tasks}/>
+     <div>
+       <form onSubmit={this.handleSubmit}>
+         <input 
+          type="text" 
+          onChange={this.addTask}
+          value={this.state.input}
+          >
+          </input>
+         <p>{this.state.input}</p>
+         <button >submit</button>
+       </form>
+     </div>
     </div>
   );
+  }
 }
-
+// const SubmitTask = () => {
+//   return (
+//     <form onSubmit={() => addTaskInput(e)}>
+//     <input 
+//       type='text'
+//       placeholder='enter item'
+//       value={this.state.input}
+//       onChange={(e) => this.setState({item: e.target.value})}
+//     />
+//     </form>
+//   )
+// }
+const Card = (props) => {
+  const todoList = props.tasks.map((todo, index) => {
+    return <Todo content={todo} index={index} />
+  })
+  return (
+    <div>
+      {todoList}
+    </div>
+  )
+}
+const Todo = (props) => {
+  return(
+    <div>
+      <p>
+      {props.content}
+      </p>
+      <p>
+        {props.index}
+      </p>
+      <button>delete?</button>
+    </div>
+  )
+}
 export default App;
